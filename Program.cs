f8d2cb000bf0b287e35ae57ecaf75fe7f2b395e2
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:39:19 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HiddenMessageC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encodedPinNumber = Console.ReadLine();
+            string startPostionString = Console.ReadLine();
+            int step = Convert.ToInt32(Console.ReadLine());
+
+            string pin = "";
+            int startPostion = startPostionString[0] - 'a';
+            pin += (encodedPinNumber[startPostion] - '0').ToString() + (encodedPinNumber[startPostion + step] - '0').ToString();
+            pin += (encodedPinNumber[startPostion + 2 * step] - '0').ToString() + (encodedPinNumber[startPostion + 3 * step] - '0').ToString();
+
+            Console.WriteLine(pin);
+            Console.Read();
+        }
+    }
+}
